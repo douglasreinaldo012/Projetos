7ea5e0faadf4bd3e2a3f0899b0308fa3a00fa32d
Douglas Reinaldo <123218252+douglasreinaldo012@users.noreply.github.com>
2023-08-04 23:41:09 -0300

    up

---
+++ cardapio_onlinev1/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    cardapio.eventos.init();
+})
+var cardapio = {};
+cardapio.eventos = {
+ init: () => {
+    console.log('iniciou')
+ }
+}
+cardapio.metodos = {
+
+}
+cardapio.templates = {
+
+}
